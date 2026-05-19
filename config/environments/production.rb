79393daee76a12184b495031a7ab557d08cd0e43
ClownViper <kikikaikai.user@gmail.com>
2026-05-19 23:50:52 +0900

    Add production SolidCache database configuration for solid_cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
+  # Use SolidCache for durable cache storage.
+  config.solid_cache.connects_to = { database: { writing: :cache } }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
